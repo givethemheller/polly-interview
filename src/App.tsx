e6943c567fdf73d3a1ba41932f09bed95be44b99
Tom Heller <tom@cannabinder.com>
2019-07-26 08:32:34 -0700

    first commit - React-TS scaffold

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+export interface IAppProps {}
+
+export default class App extends React.Component<IAppProps> {
+  public render() {
+    return <div>Polly Interview</div>;
+  }
+}
